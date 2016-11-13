640ceea2fcf61d8a38b24b2c5e78dc3dcffe463f
Joshua Lee <joshualee@iconfigure.org>
2016-11-12 15:35:16 -1000

    load function created within file

---
+++ js/calculator.js
@@ -0,0 +1,29 @@
+var calculator = (function () {
+  let _cache = 0;
+  let _total = 0;
+
+  function _load(x) {
+    _total = x;
+
+    return _total;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
