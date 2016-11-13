ea02840836a6faf716b20987dba4ce21d4ed3c44
Joshua Lee <joshualee@iconfigure.org>
2016-11-12 16:53:42 -1000

    Updated code

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,7 +1,11 @@
 var cashReigsterModule = (function () {
 
-
-
+  function _validations(x){
+    if (typeof x !== 'number'){
+     throw new Error(`Invalid input type: ${x} is not a Number`);
+    }
+  //   return null;
+    }
 
 
 
