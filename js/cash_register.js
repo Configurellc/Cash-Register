37b5938e785f6266857b07720d1a5ad176bc7288
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-11-12 19:50:25 -1000

    updated cash register

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,24 +1,32 @@
-var cashReigster = (function () {
+var cashRegister = (function () {
 
   function _validations(x){
     if (typeof x !== 'number'){
-     throw new Error(`Invalid input type: ${x} is not a Number`);
+     throw new Error(`Invalid input type: ${x} is not a number`);
     }
   //return null;
   }
 
-  function _clear(x){
-    _total = 0;
+  document.getElementById("buttonSeven").addEventListener("click", seven());
+
+  function seven(){
+    console.log("7.00");
   }
 
+  function _clear(x){
+    _total = 0.00;
+  }
 
+  function _getBalance(x){
 
+  }
 
+  function _depositCash(x){
 
+  }
 
+  function _withdrawCash(x){
 
-
-
-
+  }
 
 })();
