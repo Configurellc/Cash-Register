236d91509f37bd612e1a769b7ea3c561d9dcbbf4
Joshua Lee <joshualee@iconfigure.org>
2016-11-16 00:01:29 -1000

    updated code with the ability to add numbers together in console log using myArray.

--- js/cash_register.js
+++ js/cash_register.js
@@ -27,62 +27,134 @@ var cashRegister = (function () {
 
 })();
 
-document.getElementById("buttonSeven").addEventListener("click", seven);
+var myButtons = (function() {
+  var myArray = [];
 
-function seven() {
- document.getElementById("display").innerHTML = "7";
-}
+  document.getElementById("buttonSeven").addEventListener("click", seven);
+    function seven() {
+     document.getElementById("display").innerHTML = "7";
+     // var myArray = [];
+      myArray.push('7');
+      console.log('seven', myArray);
 
-document.getElementById("buttonEight").addEventListener("click", eight);
+    }
 
-function eight() {
- document.getElementById("display").innerHTML = "8";
-}
+  document.getElementById("buttonEight").addEventListener("click", eight);
+    function eight() {
+     document.getElementById("display").innerHTML = "8";
+     myArray.push('8');
+    }
 
-document.getElementById("buttonNine").addEventListener("click", nine);
+  document.getElementById("buttonNine").addEventListener("click", nine);
+    function nine() {
+     document.getElementById("display").innerHTML = "9";
+      myArray.push('9');
+    }
 
-function nine() {
- document.getElementById("display").innerHTML = "9";
-}
+  document.getElementById("buttonDivide").addEventListener("click", divide);
+    function divide() {
+     document.getElementById("display").innerHTML = "/";
+    }
 
-document.getElementById("buttonDivide").addEventListener("click", divide);
+  document.getElementById("buttonClear").addEventListener("click", clear);
+    function clear() {
+     document.getElementById("display").innerHTML = "0";
+      myArray.push('0');
+    }
 
-function divide() {
- document.getElementById("display").innerHTML = "/";
-}
+  document.getElementById("buttonFour").addEventListener("click", four);
+    function four() {
+     document.getElementById("display").innerHTML = "4";
+      myArray.push('4');
+    }
 
-document.getElementById("buttonClear").addEventListener("click", clear);
+  document.getElementById("buttonFive").addEventListener("click", five);
+    function five() {
+     document.getElementById("display").innerHTML = "5";
+      myArray.push('5');
+    }
 
-function clear() {
- document.getElementById("display").innerHTML = "0";
-}
+  document.getElementById("buttonSix").addEventListener("click", six);
+    function six() {
+     document.getElementById("display").innerHTML = "6";
+      myArray.push('6');
+    }
 
-document.getElementById("buttonFour").addEventListener("click", four);
+  document.getElementById("buttonMultiply").addEventListener("click", multiply);
+    function multiply() {
+     document.getElementById("display").innerHTML = "*";
+    }
 
-function four() {
- document.getElementById("display").innerHTML = "4";
-}
+  document.getElementById("buttonGetBalance").addEventListener("click", getBalance);
+    function getBalance() {
+     document.getElementById("display").innerHTML = " ";
+    }
 
-document.getElementById("buttonFive").addEventListener("click", five);
+  document.getElementById('buttonOne').addEventListener("click", one);
+    function one() {
+      document.getElementById("display").innerHTML = "1";
+       myArray.push('1');
+    }
 
-function five() {
- document.getElementById("display").innerHTML = "5";
-}
+  document.getElementById('buttonTwo').addEventListener("click", two);
+    function two() {
+      document.getElementById("display").innerHTML = "2";
+       myArray.push('2');
+    }
 
-document.getElementById("buttonSix").addEventListener("click", six);
+  document.getElementById("buttonThree").addEventListener("click", three);
+    function three() {
+      document.getElementById("display").innerHTML = "3";
+       myArray.push('3');
+    }
 
-function six() {
- document.getElementById("display").innerHTML = "6";
-}
+  document.getElementById("buttonSubtract").addEventListener("click", subtract);
+    function subtract() {
+      document.getElementById("display").innerHTML = " correct code";
+    }
 
-document.getElementById("buttonMultiply").addEventListener("click", multiply);
+  document.getElementById("buttonDepositCash").addEventListener("click", deposit);
+    function deposit() {
+      document.getElementById("display").innerHTML = "Correct code";
+    }
 
-function multiply() {
- document.getElementById("display").innerHTML = "*";
-}
+  document.getElementById("buttonZero").addEventListener("click", zero);
+    function zero() {
+      document.getElementById("display").innerHTML = "0";
+       myArray.push('0');
+      }
 
-document.getElementById("buttonGetBalance").addEventListener("click", getBalance);
+  document.getElementById("buttonDoubleZero").addEventListener("click", doubleZero);
+    function doubleZero() {
+      document.getElementById("display").innerHTML = "00";
+       myArray.push('00');
+       console.log('second', myArray);
+    }
 
-function getBalance() {
- document.getElementById("display").innerHTML = " ";
-}
+  document.getElementById("buttonDecimal").addEventListener("click", decimal);
+    function decimal() {
+      document.getElementById("display").innerHTML = ".";
+    }
+
+  document.getElementById("buttonAdd").addEventListener("click", plus);
+    function plus() {
+      document.getElementById("display").innerHTML = "+";
+    }
+
+  document.getElementById("buttonWithdrawCash").addEventListener("click", withdraw);
+    function withdraw() {
+      document.getElementById("display").innerHTML = "Fix code";
+    }
+    document.getElementById('display').addEventListener('click', displayConcat);
+     function displayConcat() {
+       var mycon = myArray.join('');
+       document.getElementById('display').innerHTML = mycon;
+       console.log("three", mycon);
+      }
+
+
+
+
+
+})();
+
